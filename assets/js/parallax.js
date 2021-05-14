52e8ddb0a3c071973ce89297c097256568bd9031
Shehan Kulathilake <shehanhere@gmail.com>
2021-05-15 03:29:56 +0530

    parallax header and navigation ui

---
+++ assets/js/parallax.js
@@ -0,0 +1,24 @@
+
+var Parallax = function(speed) {
+    this.speed = speed || 0.5;
+    this.lastScrollPos = 0;
+}
+
+Parallax.prototype.slowDown = function(element) {
+    element.style.backgroundPositionY = window.scrollY*this.speed*(-1) + 'px';
+}
+
+Parallax.prototype.isScrollingDown = function() {
+    var isDown = this.lastScrollPos < window.pageYOffset;
+    this.lastScrollPos = window.pageYOffset;
+    return isDown;
+}
+
+Parallax.prototype.panWithMouse = function(element,dampingX=1,dampingY=1) {
+
+    element.onmousemove = function(e){
+        element.style.backgroundPositionX = e.clientX*this.speed*dampingX + 'px';
+        element.style.backgroundPositionY = e.clientY*this.speed*dampingY + 'px';
+    }.bind(this);
+
+}
