1e7c29b46a3652ba212253495fdd9135d1f0999e
Shehan Kulathilake <shehanhere@gmail.com>
2021-05-17 02:29:42 +0530

    shooting star random colors and angles

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -1,7 +1,10 @@
 
 var Stars = function(container){
     this.container = container;
-    
+    this.gradients = ['star-hot','star-cold', 'star-radioactive'];
+    this.inclinations = [
+        'sky-incl-45','sky-incl-60','sky-incl-180','sky-incl-neg-180'
+    ]
 }
 
 // Generate stars on the container;
@@ -13,13 +16,18 @@ Stars.prototype.randomStars = function(){
         skyBox.classList.add('sky-box');
         sky.classList.add('sky');
         star.classList.add('star');
-        star.style.top = Math.floor(Math.random()*100) + '%';
-        star.style.left= Math.floor(Math.random()*100) + '%';
+        star.style.top = '25%' ||Math.floor(Math.random()*100) + '%';
+        star.style.left= '30%' ||Math.floor(Math.random()*100) + '%';
         sky.appendChild(star);
         skyBox.appendChild(sky);
         this.container.insertBefore(skyBox,this.container.lastElementChild);
 
-        setInterval(function(){
+        setInterval(() =>{
+            var random  = Math.floor(Math.random()*100);
+            var colorIndex = random%this.gradients.length;
+            var inclIndex = random%this.inclinations.length;
+            star.classList.toggle(this.gradients[colorIndex]);
+            sky.classList.toggle(this.inclinations[inclIndex]);
             star.style.top = Math.floor(Math.random()*100) + '%';
             star.style.left = Math.floor(Math.random()*100) + '%'
         },2000)
