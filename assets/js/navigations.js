af0873085d55f9ebe25c041c5bd4dcef54b007b3
Shehan Kulathilake <shehanhere@gmail.com>
2021-05-16 20:29:43 +0530

    glass sticky bar

--- assets/js/navigations.js
+++ assets/js/navigations.js
@@ -2,27 +2,25 @@ var toggle = document.getElementById('nav-toggle');
 var icon = document.getElementById('nav-toggle-icon');
 var navItems = document.querySelector('.navigation ul');
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    console.log(navItems.style);
-});
-
 toggle.onclick = toggleHandler;
+var isOpen;
+
 
 function toggleHandler(){;
     toggle.classList.toggle('animate__rotateIn');
-    icon.classList.toggle('fa-window-close')
-    navItems.classList.toggle('animate__fadeIn')
-    navItems.classList.toggle('animate__fadeOut')
+    icon.classList.toggle('fa-window-close');
+    navItems.classList.toggle('animate__fadeIn');
+    navItems.classList.toggle('animate__fadeOut');
     if(navItems.style.height==='0vh' || navItems.style.height===''){
-        navItems.style.display = 'flex'
-        navItems.style.height = '100vh'
+        isOpen=true;
+        navItems.style.display = 'flex';
+        navItems.style.height = '100vh';
         navItems.classList.add('navigation-mobile');
-        navItems.classList.add('animate__fadeIn')
-        navItems.classList.remove('animate__fadeOut')
+        navItems.classList.add('animate__fadeIn');
+        navItems.classList.remove('animate__fadeOut');
     } else {
-        navItems.classList.add('animate__fadeOut')
-        navItems.classList.remove('animate__fadeIn')
+        navItems.classList.add('animate__fadeOut');
+        navItems.classList.remove('animate__fadeIn');
         navItems.style.height = '0vh';
         
     }
